5bdfa8a1623a07d242dfe940251655ed57bd0bdf
sohilr <sohilr@umich.edu>
2023-01-28 19:13:50 -0500

    podium update

--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -9,13 +9,15 @@ export default function Friends() {
                 <div id="podium">
                     <div className="step" id="second">
                          <p className="var-name">Variable 1</p>
+                         <img src="/imgs/2nd-place-svgrepo-com.svg" class="overlay-image"></img>
                     </div>
                     <div className="step" id="first">
                         <p className="var-name">Variable 2</p>
-                        <img src="/imgs/first_place.svg" class="overlay-image"></img>
+                        <img src="/imgs/1st-place-svgrepo-com.svg" class="overlay-image"></img>
                     </div>
                     <div className="step" id="third">
                         <p className="var-name">Variable 3</p>
+                        <img src="/imgs/3rd-place-svgrepo-com.svg" class="overlay-image"></img>
                     </div>
                 </div>
             </main>
